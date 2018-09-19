19ba27a87b494b59a29b9a6cdaa7a4ea1bde7ebe
Jose Vergara <joss1091@gmail.com>
2018-09-19 10:50:56 -0500

    fix

--- app.js
+++ app.js
@@ -282,11 +282,11 @@ omxp.on('finish', function() {
   verifyStatus()
   playbackPlayerMutation()
 });
-omxp.on("changeStatus", function(status){
-  let playerStatus = status != "Playing" ? "inactive" : "active"
+omxp.on("changeStatus", function(info){
+  let status = info.status != "Playing" ? "inactive" : "active"
   console.log(status)
-  statusMutation(playerStatus)
-  if(status != "Playing"){
+  statusMutation(status)
+  if(info.status != "Playing"){
     console.log("iniciando player")
     playbackPlayerMutation()
   }
