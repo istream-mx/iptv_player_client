edb81163a37476901ec8c6ab0ce0716a07c15ee1
Jose Vergara <joss1091@gmail.com>
2018-12-21 15:45:33 -0500

    se agrega configuracion para vc

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,20 +8,20 @@ module.exports = {
     error_file: 'error.log',
     env: {
       NODE_ENV: 'development',
-      TENANT: "Y2FuYWw2",
+      TENANT: "dmM=",
       PLATFORM: "raspberry",
       PUBLIC_IP_SERVICE: "http://ip-api.com/json",
       SECONDARY_PUBLIC_IP_SERVICE: "http://ipinfo.io/json",
-      GRAPHQL_ENDPOINT: 'ws://canal6.iptv.tvstream.mx/api/socket',
+      GRAPHQL_ENDPOINT: 'ws://vc.iptv.tvstream.mx/api/socket',
       SCRIPT_VERSION: "1.0-test"
     },
     env_production : {
       NODE_ENV: 'production',
-      TENANT: "Y2FuYWw2",
+      TENANT: "dmM=",
       PLATFORM: "raspberry",
       PUBLIC_IP_SERVICE: "http://ip-api.com/json",
       SECONDARY_PUBLIC_IP_SERVICE: "http://ipinfo.io/json",
-      GRAPHQL_ENDPOINT: 'ws://canal6.iptv.tvstream.mx/api/socket',
+      GRAPHQL_ENDPOINT: 'ws://vc.iptv.tvstream.mx/api/socket',
       SCRIPT_VERSION: "1.2.2"
     }
   },
@@ -32,15 +32,15 @@ module.exports = {
     error_file: "/dev/null",
     env_production : {
       NODE_ENV: 'production',
-      TENANT: "Y2FuYWw2",
+      TENANT: "dmM=",
       PLATFORM: "raspberry",
-      GRAPHQL_ENDPOINT: 'ws://canal6.iptv.tvstream.mx/api/socket'
+      GRAPHQL_ENDPOINT: 'ws://vc.iptv.tvstream.mx/api/socket'
     },
     env: {
       NODE_ENV: 'development',
-      TENANT: "Y2FuYWw2",
+      TENANT: "dmM=",
       PLATFORM: "raspberry",
-      GRAPHQL_ENDPOINT: 'ws://canal6.iptv.tvstream.mx/api/socket'
+      GRAPHQL_ENDPOINT: 'ws://vc.iptv.tvstream.mx/api/socket'
     }
   }
 ],
@@ -50,7 +50,7 @@ module.exports = {
       user : 'pi',
       host : 'localhost',
       ref  : 'origin/master',
-      repo : 'http://159.89.43.103/tvstream/iptv-client.git',
+      repo : 'http://159.89.43.103/tvstream/clients/vc/iptv-client.git',
       path: '/home/pi/Documents/production',
       'post-deploy' : 'yarn && pm2 startOrRestart ecosystem.config.js --env production --update-env'
     },
@@ -58,7 +58,7 @@ module.exports = {
       user : 'pi',
       host : 'localhost',
       ref  : 'origin/develop',
-      repo : 'http://159.89.43.103/tvstream/iptv-client.git',
+      repo : 'http://159.89.43.103/tvstream/clients/vc/iptv-client.git',
       path : '/home/pi/Documents/production',
       'post-deploy' : 'yarn && pm2 startOrRestart ecosystem.config.js --env production --update-env'
     }
