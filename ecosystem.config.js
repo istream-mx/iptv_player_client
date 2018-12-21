7869fd9d03c2f91a2b213d23daccedc50eec5c95
Jose Vergara <joss1091@gmail.com>
2018-12-21 16:04:36 -0500

    se cambia para canal6

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       PLATFORM: "raspberry",
       PUBLIC_IP_SERVICE: "http://ip-api.com/json",
       SECONDARY_PUBLIC_IP_SERVICE: "http://ipinfo.io/json",
-      GRAPHQL_ENDPOINT: 'ws://vc.iptv.tvstream.mx/api/socket',
+      GRAPHQL_ENDPOINT: 'ws://canal6.iptv.tvstream.mx/api/socket',
       SCRIPT_VERSION: "1.0-test"
     },
     env_production : {
@@ -21,7 +21,7 @@ module.exports = {
       PLATFORM: "raspberry",
       PUBLIC_IP_SERVICE: "http://ip-api.com/json",
       SECONDARY_PUBLIC_IP_SERVICE: "http://ipinfo.io/json",
-      GRAPHQL_ENDPOINT: 'ws://vc.iptv.tvstream.mx/api/socket',
+      GRAPHQL_ENDPOINT: 'ws://canal6.iptv.tvstream.mx/api/socket',
       SCRIPT_VERSION: "1.2.2"
     }
   },
@@ -34,13 +34,13 @@ module.exports = {
       NODE_ENV: 'production',
       TENANT: "dmM=",
       PLATFORM: "raspberry",
-      GRAPHQL_ENDPOINT: 'ws://vc.iptv.tvstream.mx/api/socket'
+      GRAPHQL_ENDPOINT: 'ws://canal6.iptv.tvstream.mx/api/socket'
     },
     env: {
       NODE_ENV: 'development',
       TENANT: "dmM=",
       PLATFORM: "raspberry",
-      GRAPHQL_ENDPOINT: 'ws://vc.iptv.tvstream.mx/api/socket'
+      GRAPHQL_ENDPOINT: 'ws://canal6.iptv.tvstream.mx/api/socket'
     }
   }
 ],
@@ -50,7 +50,7 @@ module.exports = {
       user : 'pi',
       host : 'localhost',
       ref  : 'origin/master',
-      repo : 'http://159.89.43.103/tvstream/clients/vc/iptv-client.git',
+      repo : 'git@159.89.43.103:tvstream/iptv-client.git',
       path: '/home/pi/Documents/production',
       'post-deploy' : 'yarn && pm2 startOrRestart ecosystem.config.js --env production --update-env'
     },
@@ -58,7 +58,7 @@ module.exports = {
       user : 'pi',
       host : 'localhost',
       ref  : 'origin/develop',
-      repo : 'http://159.89.43.103/tvstream/clients/vc/iptv-client.git',
+      repo : 'git@159.89.43.103:tvstream/iptv-client.git',
       path : '/home/pi/Documents/production',
       'post-deploy' : 'yarn && pm2 startOrRestart ecosystem.config.js --env production --update-env'
     }
